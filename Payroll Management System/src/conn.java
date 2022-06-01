d5250fb8083ae3a3c1bc3f2f992fa7ab59d4b43d
Rohit Joshi <86643003+Mranonymous16@users.noreply.github.com>
2022-06-02 00:50:56 +0530

    Add files via upload

---
+++ Payroll Management System/src/conn.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+public class conn {
+    
+    public Connection c;
+    public Statement s;
+ 
+    public conn(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            c=DriverManager.getConnection("jdbc:mysql:///payroll","root","");
+            
+        
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+ 
